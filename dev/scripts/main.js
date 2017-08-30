41d81455ec895df9f4e124687b239e63650c2ee2
Stewart Gottfried <stewart.gottfried@gmail.com>
2017-08-30 14:06:46 -0400

    added class ID console log based on dropdown selection

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -10,14 +10,27 @@ quiz.category = function(category){
 		categoryNames.forEach(function(categoryName){
 			// quiz.injectCategory(categoryName);
 			$('.categories').append($('<option>').text(categoryName.name).val(categoryName.id));
+			quiz.questionNumber();
+			// console.log(categoryName);
 		});
 	});
 }
 
-quiz.injectCategory = function(cat){
-	console.log(cat);
-	
-}
+quiz.questionNumber = function(){
+	$('.categories').on('change', function(){
+		var catId = $('.categories').val();
+		console.log(catId);
+	});
+	// $.ajax({
+	// 	url: `https://opentdb.com/api_count.php?category=${catId}`,
+	// 	method:'GET',
+	// 	dataType:'json'
+	// }).then(function(res){
+	// 	// let categoryId = catId.id;
+	// 	console.log(res);
+	// });
+
+};
 
 
 
